9272cc441d5c490eb8cc8d887825e6274c18d9cb
Abram <58718191+4b3c@users.noreply.github.com>
2024-03-14 09:06:50 -0700

    trying to change the movement to rely on mouse movements

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.awt.event.MouseEvent;
+
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
@@ -7,6 +9,7 @@ import javafx.application.Application;
 import javafx.geometry.Point3D;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
@@ -66,24 +69,26 @@ public class Main extends Application {
     }
 
     private void handleInput(Scene scene, Rotate xRotate, Rotate yRotate) {
-        scene.setOnKeyPressed(event -> {
-            KeyCode keyCode = event.getCode();
-            switch (keyCode) {
-                case UP:
-                	xRotate.setAngle(xRotate.getAngle() + ROTATION_SPEED);
-                    break;
-                case DOWN:
-                	xRotate.setAngle(xRotate.getAngle() - ROTATION_SPEED);
-                    break;
-                case LEFT:
-                	yRotate.setAngle(yRotate.getAngle() + ROTATION_SPEED);
-                    break;
-                case RIGHT:
-                	yRotate.setAngle(yRotate.getAngle() - ROTATION_SPEED);
-                    break;
+    	final boolean pressed;
+        scene.setOnMousePressed(event -> {
+        	MouseButton mouseButton = event.getButton();
+            switch (mouseButton) {
+                case PRIMARY:
+                	pressed = true;
                 default:
                     break;
             }
+        });
+        
+        
+        scene.setOnMouseReleased(event -> {
+        	MouseButton mouseButton = event.getButton();
+        	switch (mouseButton) {
+        		case PRIMARY:
+        			int x = 10;
+        		default:
+        			break;
+        	}
         });
     }
 
