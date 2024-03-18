34495ee38127f0cd19c8a062a486e156146f5055
Abram <58718191+4b3c@users.noreply.github.com>
2024-03-17 20:51:49 -0700

    cleaned up the code a little bit

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,6 +4,7 @@ import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.geometry.Point3D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -11,8 +12,6 @@ import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
 import javafx.scene.shape.Cylinder;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.LineTo;
 import javafx.scene.shape.Shape3D;
 import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
@@ -28,12 +27,6 @@ public class Main extends Application {
     public static Rotate xRotate = new Rotate(0, Rotate.X_AXIS);
     public static Rotate yRotate = new Rotate(0, Rotate.Y_AXIS);
     public static Rotate zRotate = new Rotate(0, Rotate.Z_AXIS);
-    public static Rotate xRotate90 = new Rotate(90, Rotate.X_AXIS);
-    public static Rotate yRotate90 = new Rotate(90, Rotate.Y_AXIS);
-    public static Rotate zRotate90 = new Rotate(90, Rotate.Z_AXIS);
-    
-    public static Rotate groupRotateX = new Rotate(0, Rotate.X_AXIS);
-    public static Rotate groupRotateY = new Rotate(0, Rotate.Y_AXIS);
 
     @Override
     public void start(Stage primaryStage) {
@@ -41,6 +34,8 @@ public class Main extends Application {
     	// Define text areas for debugging mouse input, and basic things for the window
         Text mousePosText = new Text(10, 20, null);
         Text lastMousePosText = new Text(10, 40, null);
+        
+        
         BorderPane root = new BorderPane();
         Scene scene = new Scene(root, 1200, 800);
         
@@ -48,25 +43,28 @@ public class Main extends Application {
         Box box = new Box(50, 100, 200);
         setMaterial(box);
         box.setTranslateX(300);
+        box.setTranslateY(20);
+        box.setTranslateZ(50);
         
         // Define some cylinders to act as axis for the 3D space
         Cylinder cylinderX = new Cylinder(2, 750);
         Cylinder cylinderY = new Cylinder(2, 750);
         Cylinder cylinderZ = new Cylinder(2, 750);
-        setCenter(cylinderX);
-        setCenter(cylinderY);
-        setCenter(cylinderZ);
-        cylinderX.getTransforms().addAll(xRotate, yRotate90, zRotate90);
-        cylinderY.getTransforms().addAll(xRotate, yRotate, zRotate90);
-        cylinderZ.getTransforms().addAll(xRotate, yRotate, zRotate);
+
+        cylinderX.setRotationAxis(new Point3D(1, 0, 0));
+        cylinderX.setRotate(90);
+        cylinderY.setRotationAxis(new Point3D(0, 1, 0));
+        cylinderY.setRotate(90);
+        cylinderZ.setRotationAxis(new Point3D(0, 0, 1));
+        cylinderZ.setRotate(90);
         
-        Group group = new Group(box);
+        Group group = new Group(box, cylinderX, cylinderY, cylinderZ);
         group.setTranslateX(600);
         group.setTranslateY(400);
         group.getTransforms().addAll(xRotate, yRotate, zRotate);
         
         
-        root.getChildren().addAll(mousePosText, lastMousePosText, cylinderX, cylinderY, cylinderZ, group);
+        root.getChildren().addAll(mousePosText, lastMousePosText, group);
 
             
         // Set up the game loop
@@ -103,8 +101,6 @@ public class Main extends Application {
     	if (mouseClicked) {
     		xRotate.setAngle(xRotate.getAngle() + (mousePos[1] - lastMousePos[1]) / 2);
     		yRotate.setAngle(yRotate.getAngle() - (mousePos[0] - lastMousePos[0]) / 2);
-    		xRotate90.setAngle(xRotate.getAngle() + 90);
-    		yRotate90.setAngle(yRotate.getAngle() + 90);
     		updateLastMousePos();
     	}
     }
